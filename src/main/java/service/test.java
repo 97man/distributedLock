2c09a7e1f07d0bfc7a6e5c922fd6b7b7c4a21f59
dingzhenbj <dingzhenbj@fenbi.com>
2021-01-18 12:43:27 +0800

    测试分支·

---
+++ src/main/java/service/test.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)test.java, 1月 18, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package service;
+
+/**
+ * @author dingzhen22
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试分支");
+    }
+}
